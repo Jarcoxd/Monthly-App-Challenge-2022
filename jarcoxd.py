d2ffaa7c416dd5c8cb02660add8a4f8e5aa815fd
Jarcoxd <jarquin0721@gmail.com>
2025-02-15 17:39:14 -0600

    Create jarcoxd.py

---
+++ jarcoxd.py
@@ -0,0 +1,33 @@
+# https://.python.org
+
+# Comentario en línea python
+
+"""
+Así se crean los comentarios
+multilínea en python
+"""
+pri
+También podemos usar comillas simples
+para comentarios multilínea
+'''
+
+# Variables en python
+
+my_variable = "Variable de Jarco"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANTE = "Mi constante" # por convención
+
+my_int = 1 
+my_float = 1.5
+my_bool = True
+my_bool = False 
+my_string = "Mi cadena de texto"
+my_other_string = 'Mi otra cadena de texto'
+
+print("¡Hola, Python!") 
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
